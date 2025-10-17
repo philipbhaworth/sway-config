986f1280c59c861a73f55f598cf586eec6ccfb5a
philipbhaworth <philipbhaworth@gmail.com>
2025-10-17 10:41:16 -0500

    updating config

--- sway-lab/.config/sway/scripts/powermenu.sh
+++ sway-lab/.config/sway/scripts/powermenu.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 # ~/.config/sway/scripts/powermenu.sh
-
 entries="⏻  Shutdown\n⭮ Reboot\n⏾ Suspend\n🔒 Lock\n⇠ Logout"
-
 selected=$(echo -e "$entries" | wofi --dmenu \
     --prompt "Power Menu" \
     --width 250 \
     --height 210 \
     --cache-file /dev/null \
     | awk '{print tolower($2)}')
-
 case $selected in
     shutdown)
         systemctl poweroff
@@ -21,7 +18,27 @@ case $selected in
         systemctl suspend
         ;;
     lock)
-        swaylock -f
+        swaylock -f \
+            --image ~/wallpapers/wallpapers/wall72.png \
+            --color 100f0f \
+            --inside-color 282726 \
+            --inside-clear-color 24837b \
+            --inside-ver-color ad8301 \
+            --inside-wrong-color af3029 \
+            --ring-color cecdc3 \
+            --ring-clear-color 24837b \
+            --ring-ver-color ad8301 \
+            --ring-wrong-color af3029 \
+            --key-hl-color 24837b \
+            --bs-hl-color af3029 \
+            --separator-color 100f0f \
+            --text-color cecdc3 \
+            --text-clear-color cecdc3 \
+            --text-ver-color cecdc3 \
+            --text-wrong-color cecdc3 \
+            --indicator-radius 100 \
+            --indicator-thickness 10 \
+            --font "JetBrainsMono Nerd Font"
         ;;
     logout)
         swaymsg exit
